ba7bfeae2a8c0d5e3f38cfa96ce752377aceae0d
김도형 <shapekim98@gmail.com>
2025-04-28 19:29:59 +0900

    [feat] #4 탭바 연결

--- RecapStore/ContentView.swift
+++ RecapStore/ContentView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State
+    private var currentTabId: Int = 4
+    
     var body: some View {
-        MyAppView(viewModel: MyAppViewModel())
+        TabView(selection: $currentTabId) {
+            VStack{
+                Spacer()
+            }
+            .tabItem {
+                Image(systemName: "doc.text.image")
+                
+                Text("투데이")
+            }
+            .tag(0)
+            
+            VStack{
+                Spacer()
+            }
+            .tabItem {
+                Image(.rocket)
+                
+                Text("게임")
+            }
+            .tag(1)
+            
+            MyAppView(viewModel: MyAppViewModel())
+                .tabItem {
+                    Image(systemName: "square.stack.3d.up.fill")
+                    
+                    Text("앱")
+                }
+                .tag(2)
+            
+            VStack{
+                Spacer()
+            }
+            .tabItem {
+                Image(systemName: "arcade.stick")
+                
+                Text("아케이드")
+            }
+            .tag(3)
+            
+            SearchView(viewModel: SearchViewModel())
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    
+                    Text("검색")
+                }
+                .tag(4)
+        }
     }
 }
 
